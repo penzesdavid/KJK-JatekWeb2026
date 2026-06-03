7ca0eea9fc08f3177be85ce1d55560b0095ddfa8
penzesdavid <penzes.david5@gmail.com>
2026-06-03 09:54:30 +0200

    2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/https://penzesdavid.github.io/KJK-JatekWeb2026/', // <--- Ezt a sort add hozzá a saját repód nevével!
+  plugins: [react(), tailwindcss()],
+  // ... a többi config változatlan marad
   plugins: [react(), tailwindcss()],
   esbuild: {
     loader: 'jsx',
@@ -16,3 +19,4 @@ export default defineConfig({
     watch: process.env.DISABLE_HMR === 'true' ? null : {},
   }
 });
+  
